08ab3ac37c881eec7a76b20469cb2e2ddea7f0f3
brijkishorgupta <brijkishorgupta290@gmail.com>
2026-02-14 22:56:54 +0530

    added

--- parent_module1/main.tf
+++ parent_module1/main.tf
@@ -1,6 +1,5 @@
 module "rgs" {
   source = "../child_module/rgs"
-
   rgs = var.rgs
 
 }
@@ -30,49 +29,15 @@ module "public_ip" {
 
 }
 
-# module "frontend_vm" {
-#     depends_on = [ module.frontend_subnet ]
-#   source                 = "../Module/vm"
-#   nic_name               = "nic-frontend"
-#   location               = "centralindia"
-#   resource_group_name    = "rg-brij"
-#   subnet_id              = "/subscriptions/becf41a0-446d-42d0-92b7-09e1ab420ad2/resourceGroups/rg-brij/providers/Microsoft.Network/virtualNetworks/vnet-brij/subnets/frontend_subnet"
-#   vm_name                = "vm-brij"
-#   vm_size                = "Standard_B2s"
-#   admin_username         = "adminbrij"
-#   admin_password         = "Khushali@1987"
-#   image_publisher        = "Canonical"
-#   image_offer            = "UbuntuServer"
-#   image_sku              = "18.04-LTS"
-#   image_version          = "latest"
-# }
-# module "backend_vm" {
-#     depends_on = [ module.backend_subnet ]
-#   source                 = "../Module/vm"
-#   nic_name               = "nic-backend"
-#   location               = "centralindia"
-#   resource_group_name    = "rg-brij"
-#   subnet_id              = "/subscriptions/becf41a0-446d-42d0-92b7-09e1ab420ad2/resourceGroups/rg-brij/providers/Microsoft.Network/virtualNetworks/vnet-brij/subnets/backend_subnet"
-#   vm_name                = "vm-backend"
-#   vm_size                = "Standard_B2s"
-#   admin_username         = "adminbrij"
-#   admin_password         = "Khushali@1987"
-#   image_publisher        = "Canonical"
-#   image_offer            = "UbuntuServer"
-#   image_sku              = "18.04-LTS"
-#   image_version          = "latest"
+module "network_interface" {
+  depends_on = [module.rgs, module.network, module.subnet, module.public_ip]
 
-# }
-# module "sql_server" {
-#   source                 = "../Module/sql_server"
-#   sql_server_name        = "sqlserverbrij"
-#   resource_group_name    = "rg-brij"
-#   location               = "centralindia"
-#   administrator_login          = "adminbrij"
-#   administrator_login_password = "Khushali@1987"
-# }
-# module "sql_database" {
-#   source                 = "../Module/sql_database"
-#   sql_database_name      = "sqldbbrij"
-#   sql_server_id          = "/subscriptions/becf41a0-446d-42d0-92b7-09e1ab420ad2/resourceGroups/rg-brij/providers/Microsoft.Sql/servers/sqlserverbrij"
-# }
+  source = "../child_module/network_interface"
+  nic = var.nic
+}
+module "virtual_machine" {
+  depends_on = [ module.rgs, module.network, module.subnet, module.network_interface, module.public_ip ]
+
+  source = "../child_module/virtual_machin"
+  vm = var.vm  
+}
--- parent_module1/variable.tf
+++ parent_module1/variable.tf
@@ -8,31 +8,36 @@ variable "rgs" {
 }
 variable "vnet_name" {
 
-  type = map(object({
-    name                = string
-    resource_group_name = string
-    location            = string
-    address_space       = list(string)
-  }))
+  # type = map(object({
+  #   name                = string
+  #   resource_group_name = string
+  #   location            = string
+  #   address_space       = list(string)
+  # }))
 }
 
 variable "subnet" {
 
-  type = map(object({
-    name                 = string
-    resource_group_name  = string
-    virtual_network_name = string
-    address_prefixes     = list(string)
-  }))
+  # type = map(object({
+  #   name                 = string
+  #   resource_group_name  = string
+  #   virtual_network_name = string
+  #   address_prefixes     = list(string)
+  # }))
 }
 variable "pip_name" {
 
-  type = map(object({
+  # type = map(object({
 
-    name                = string
-    resource_group_name = string
-    location            = string
-    allocation_method   = string
-  }))
+  #   name                = string
+  #   resource_group_name = string
+  #   location            = string
+  #   allocation_method   = string
+  # }))
 
 }
+  variable "nic" {}
+  variable "vm" {}
+#   variable "network_interface_ids" {
+#   type = list(string)
+# }

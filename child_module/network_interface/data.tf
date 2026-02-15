451f9d3c48afe64747b4dc60cb1726b984a9f358
brijkishorgupta <brijkishorgupta290@gmail.com>
2026-02-15 13:34:13 +0530

    added

--- child_module/network_interface/data.tf
+++ child_module/network_interface/data.tf
@@ -1,8 +1,13 @@
 data "azurerm_subnet" "subnet" {
-  name                 = "brij-subnet"
+  name                 = "frontend-subnet"
   virtual_network_name = "brij-vnet"
   resource_group_name  = "brij1"
 }
+# data "azurerm_subnet" "subnet" {
+#   name                 = "backend-subnet"
+#   virtual_network_name = "brij-vnet"
+#   resource_group_name  = "brij1"
+# }
 data "azurerm_public_ip" "pip" {
   name                = "brij-pip"
   resource_group_name = "brij1"
